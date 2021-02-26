566f5152a233cf5cf6804b0106967773995d35c0
hstrada <h.strada@hotmail.com>
2021-02-25 21:36:24 -0300

    chore: add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
 var express = require("express");
-var { graphqlHTTP } = require("express-graphql");
 
 const itemTypeDefs = require("./items/item.type-def");
-import { makeExecutableSchema } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 import { itemResolver } from "./items/item.resolver";
 
-const schema = makeExecutableSchema({
+var app = express();
+
+const server = new ApolloServer({
   typeDefs: itemTypeDefs,
   resolvers: itemResolver,
 });
 
-var app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+server.applyMiddleware({ app });
 
 app.listen(4000);
 console.log("Running a GraphQL API server at http://localhost:4000/graphql");
